50b10a921733244c882ba86c4b22318ba8c12d11
MarcosAvera <110677755+MarcosAvera@users.noreply.github.com>
2024-09-19 21:49:08 -0300

    Add files via upload

--- TCC/src/javascript/aulas.js
+++ TCC/src/javascript/aulas.js
@@ -1,15 +1,21 @@
-const optionMenu = document.querySelector(".acesso-rapido"),
-        botaoSlct = optionMenu.querySelector(".botaoSelect"),
-        options = optionMenu.querySelectorAll(".opcoes"),
-        btn_text = optionMenu.querySelector(".btn_text");
+document.getElementById('open_btn').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+});
 
-    selectbtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+
+
+
+const optionMenu = document.querySelector(".acesso-rapido"),
+      botaoSlct = optionMenu.querySelector(".botaoSelect"),
+      options = optionMenu.querySelectorAll(".opcoes"),
+      btn_text = optionMenu.querySelector(".btn_text");
+
+    selectBtn.addEventListener("click", ()=> optionMenu.classList.toggle("active"));
 
 options.forEach(option => {
-    option.addEventListener("click", ()=>{
+    option.addEventListener("click", ()=> {
         let selectedOption = option.querySelector(".btn_text").innerText;
         btn_text.innerText = selectedOption;
-        console.log(selectedOption)
+        console.log(selectedOption);
     })
-    
-})        
+})
